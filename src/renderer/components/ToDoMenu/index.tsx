57670c5c6a1ba276b550d3b2714a80e960429e19
elton11220 <jonesbak@outlook.com>
2022-07-04 22:39:25 +0800

    added a dialog for the method 'onAddTagItem'

--- src/renderer/components/ToDoMenu/index.tsx
+++ src/renderer/components/ToDoMenu/index.tsx
@@ -1,5 +1,12 @@
-import React, { useEffect, useMemo, memo, useState } from 'react';
-import { Menu } from 'tdesign-react';
+import React, {
+  useEffect,
+  useMemo,
+  memo,
+  useState,
+  useRef,
+  useCallback,
+} from 'react';
+import { Dialog, Form, Input, Menu } from 'tdesign-react';
 import type { MenuValue } from 'tdesign-react';
 import {
   TimeIcon,
@@ -15,8 +22,10 @@ import {
 } from 'tdesign-icons-react';
 import useTitleBarAreaRect from 'renderer/hooks/useTitleBarAreaRect';
 import styles from './style.module.scss';
+import ColorPicker from '../ColorPicker';
 
 const { MenuItem, SubMenu, MenuGroup } = Menu;
+const { FormItem } = Form;
 
 const resolveList: (
   data: ListItem[]
@@ -64,6 +73,7 @@ interface ToDoMenuProps {
   onDeleteTodoMenuItem: (itemId: any) => void;
   onDeleteTagItem: (itemId: any) => void;
   onBreakTodoMenuItemFolder: (itemId: any) => void;
+  onAddTagItem: (item: TagItem) => void;
 }
 
 const ToDoMenu: React.FC<ToDoMenuProps> = (props) => {
@@ -75,6 +85,7 @@ const ToDoMenu: React.FC<ToDoMenuProps> = (props) => {
     onDeleteTagItem,
     onDeleteTodoMenuItem,
     onBreakTodoMenuItemFolder,
+    onAddTagItem,
   } = props;
   const todoMenu = useMemo(() => resolveList(todos), [todos]);
   const [expanded, setExpanded] = useState<MenuValue[]>([]);
@@ -117,80 +128,169 @@ const ToDoMenu: React.FC<ToDoMenuProps> = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const { height: sysTitleBarHeight } = useTitleBarAreaRect();
+  const [addTagItemDialogShow, setAddTagItemDialogShow] =
+    useState<boolean>(false);
+  const onAddTagItemDialogForm = useRef<HTMLFormElement>();
+  const onAddTagItemDialogConfirm = useCallback(() => {
+    if (onAddTagItemDialogForm.current?.validate()) {
+      onAddTagItem({
+        id: new Date().getTime(),
+        ...onAddTagItemDialogForm.current.getFieldsValue(['color', 'title']),
+      });
+    }
+  }, [onAddTagItem]);
   return (
-    <div
-      onContextMenu={showContextMenu}
-      className={styles.container}
-      style={{ height: `calc(100vh - ${sysTitleBarHeight}px)` }}
-    >
-      <Menu
-        value={active}
-        onChange={(v: any) => onChange(v)}
-        defaultValue="new"
-        expanded={expanded}
-        onExpand={setExpanded}
+    <>
+      <Dialog
+        visible={addTagItemDialogShow}
+        header="添加标签"
+        confirmBtn="保存"
+        showOverlay={false}
+        destroyOnClose
+        onClose={() => {
+          setAddTagItemDialogShow(false);
+        }}
+        onConfirm={onAddTagItemDialogConfirm}
       >
-        <MenuItem value="today" icon={<BrowseIcon />}>
-          今天
-        </MenuItem>
-        <MenuItem value="recent" icon={<TimeIcon />}>
-          最近7天
-        </MenuItem>
-        <MenuItem value="new" icon={<RootListIcon />}>
-          未分类
-        </MenuItem>
-        <MenuItem value="finish" icon={<CheckCircleIcon />}>
-          已完成
-        </MenuItem>
-        <MenuItem value="trash" icon={<ClearIcon />}>
-          垃圾桶
-        </MenuItem>
-        <MenuGroup
-          title={
-            <div className={styles.menuGroupTitleContainer}>
-              <span>清单</span>
-              <AddIcon
-                size="large"
-                style={{ color: 'var(--td-text-color-placeholder)' }}
-                className={styles.icon}
-              />
-            </div>
-          }
+        <Form ref={onAddTagItemDialogForm} style={{ marginTop: '20px' }}>
+          <FormItem
+            label="名称"
+            name="title"
+            rules={[
+              { message: '标签名称不能为空', type: 'error', required: true },
+            ]}
+          >
+            <Input placeholder="标签" />
+          </FormItem>
+          <FormItem label="颜色" name="color" initialData={-1}>
+            {React.createElement(ColorPicker)}
+          </FormItem>
+        </Form>
+      </Dialog>
+      <div
+        onContextMenu={showContextMenu}
+        className={styles.container}
+        style={{ height: `calc(100vh - ${sysTitleBarHeight}px)` }}
+      >
+        <Menu
+          value={active}
+          onChange={(v: any) => onChange(v)}
+          defaultValue="new"
+          expanded={expanded}
+          onExpand={setExpanded}
         >
-          {todoMenu.map((item) => {
-            if (!item.folder) {
-              return (
-                <MenuItem value={item.id} key={item.id} icon={<ViewListIcon />}>
+          <MenuItem value="today" icon={<BrowseIcon />}>
+            今天
+          </MenuItem>
+          <MenuItem value="recent" icon={<TimeIcon />}>
+            最近7天
+          </MenuItem>
+          <MenuItem value="new" icon={<RootListIcon />}>
+            未分类
+          </MenuItem>
+          <MenuItem value="finish" icon={<CheckCircleIcon />}>
+            已完成
+          </MenuItem>
+          <MenuItem value="trash" icon={<ClearIcon />}>
+            垃圾桶
+          </MenuItem>
+          <MenuGroup
+            title={
+              <div className={styles.menuGroupTitleContainer}>
+                <span>清单</span>
+                <AddIcon
+                  size="large"
+                  style={{ color: 'var(--td-text-color-placeholder)' }}
+                  className={styles.icon}
+                />
+              </div>
+            }
+          >
+            {todoMenu.map((item) => {
+              if (!item.folder) {
+                return (
+                  <MenuItem
+                    value={item.id}
+                    key={item.id}
+                    icon={<ViewListIcon />}
+                  >
+                    <div
+                      className={styles.identifier}
+                      itemID={item.id}
+                      itemType="toDoMenuItem"
+                    >
+                      <EllipsisIcon
+                        className={styles.moreBtn}
+                        style={{
+                          color:
+                            active === item.id
+                              ? '#fff'
+                              : 'var(--td-text-color-primary)',
+                        }}
+                        onClick={() => {
+                          showContextMenuByClick('toDoMenuItem', item.id);
+                        }}
+                      />
+                    </div>
+                    <div className={styles.menuItem}>
+                      <div className={styles.title}>{item.title}</div>
+                      <div className={styles.indicators}>
+                        <div
+                          className={styles.color}
+                          style={{ backgroundColor: item?.color }}
+                        />
+                        <div
+                          className={styles.number}
+                          style={{
+                            color:
+                              active === item.id
+                                ? '#fff'
+                                : 'var(--td-text-color-primary)',
+                          }}
+                        >
+                          0
+                        </div>
+                      </div>
+                    </div>
+                  </MenuItem>
+                );
+              }
+              const children = item.children.map((child) => (
+                <MenuItem
+                  value={child.id}
+                  key={child.id}
+                  icon={<ViewListIcon />}
+                >
                   <div
                     className={styles.identifier}
-                    itemID={item.id}
+                    itemID={child.id}
                     itemType="toDoMenuItem"
                   >
                     <EllipsisIcon
                       className={styles.moreBtn}
                       style={{
                         color:
-                          active === item.id
+                          active === child.id
                             ? '#fff'
                             : 'var(--td-text-color-primary)',
                       }}
                       onClick={() => {
-                        showContextMenuByClick('toDoMenuItem', item.id);
+                        showContextMenuByClick('toDoMenuItem', child.id);
                       }}
                     />
                   </div>
-                  <div className={styles.menuItem}>
-                    <div className={styles.title}>{item.title}</div>
+                  <div className={styles.menuItem} style={{ width: '132px' }}>
+                    <div className={styles.title}>{child.title}</div>
                     <div className={styles.indicators}>
                       <div
                         className={styles.color}
-                        style={{ backgroundColor: item?.color }}
+                        style={{ backgroundColor: child?.color }}
                       />
                       <div
                         className={styles.number}
                         style={{
                           color:
-                            active === item.id
+                            active === child.id
                               ? '#fff'
                               : 'var(--td-text-color-primary)',
                         }}
@@ -200,40 +300,98 @@ const ToDoMenu: React.FC<ToDoMenuProps> = (props) => {
                     </div>
                   </div>
                 </MenuItem>
+              ));
+              return (
+                <SubMenu
+                  value={item.id}
+                  key={item.id}
+                  title={
+                    <>
+                      <div
+                        className={styles.identifier}
+                        itemID={item.id}
+                        itemType="toDoMenuItemFolder"
+                      >
+                        <EllipsisIcon
+                          className={styles.moreBtn}
+                          style={{
+                            color:
+                              !expanded.includes(item.id) &&
+                              todos.find((todo: ListItem) => todo.id === active)
+                                ?.parent === item.id
+                                ? '#fff'
+                                : 'var(--td-text-color-primary)',
+                          }}
+                          onClick={() => {
+                            showContextMenuByClick(
+                              'toDoMenuItemFolder',
+                              item.id
+                            );
+                          }}
+                        />
+                      </div>
+                      <div
+                        className={styles.menuItem}
+                        style={{ width: '140px' }}
+                      >
+                        <div className={styles.title}>{item.title}</div>
+                      </div>
+                    </>
+                  }
+                  icon={<FolderOpenIcon />}
+                >
+                  {children}
+                </SubMenu>
               );
+            })}
+          </MenuGroup>
+          <MenuGroup
+            title={
+              <div className={styles.menuGroupTitleContainer}>
+                <span>标签</span>
+                <AddIcon
+                  size="large"
+                  style={{ color: 'var(--td-text-color-placeholder)' }}
+                  className={styles.icon}
+                  onClick={() => {
+                    setAddTagItemDialogShow(true);
+                  }}
+                />
+              </div>
             }
-            const children = item.children.map((child) => (
-              <MenuItem value={child.id} key={child.id} icon={<ViewListIcon />}>
+          >
+            {tags.map((item: TagItem) => (
+              <MenuItem key={item.id} value={item.id} icon={<DiscountIcon />}>
                 <div
                   className={styles.identifier}
-                  itemID={child.id}
-                  itemType="toDoMenuItem"
+                  itemID={item.id}
+                  itemType="tagItem"
                 >
                   <EllipsisIcon
                     className={styles.moreBtn}
                     style={{
                       color:
-                        active === child.id
+                        active === item.id
                           ? '#fff'
                           : 'var(--td-text-color-primary)',
                     }}
                     onClick={() => {
-                      showContextMenuByClick('toDoMenuItem', child.id);
+                      showContextMenuByClick('tagItem', item.id);
                     }}
                   />
                 </div>
-                <div className={styles.menuItem} style={{ width: '132px' }}>
-                  <div className={styles.title}>{child.title}</div>
+                <div className={styles.menuItem}>
+                  <div className={styles.title}>{item.title}</div>
                   <div className={styles.indicators}>
                     <div
                       className={styles.color}
-                      style={{ backgroundColor: child?.color }}
+                      style={{ backgroundColor: item?.color }}
                     />
                     <div
                       className={styles.number}
                       style={{
                         color:
-                          active === child.id
+                          active === item.id
                             ? '#fff'
                             : 'var(--td-text-color-primary)',
                       }}
@@ -243,102 +401,11 @@ const ToDoMenu: React.FC<ToDoMenuProps> = (props) => {
                   </div>
                 </div>
               </MenuItem>
-            ));
-            return (
-              <SubMenu
-                value={item.id}
-                key={item.id}
-                title={
-                  <>
-                    <div
-                      className={styles.identifier}
-                      itemID={item.id}
-                      itemType="toDoMenuItemFolder"
-                    >
-                      <EllipsisIcon
-                        className={styles.moreBtn}
-                        style={{
-                          color:
-                            !expanded.includes(item.id) &&
-                            todos.find((todo: ListItem) => todo.id === active)
-                              ?.parent === item.id
-                              ? '#fff'
-                              : 'var(--td-text-color-primary)',
-                        }}
-                        onClick={() => {
-                          showContextMenuByClick('toDoMenuItemFolder', item.id);
-                        }}
-                      />
-                    </div>
-                    <div className={styles.menuItem} style={{ width: '140px' }}>
-                      <div className={styles.title}>{item.title}</div>
-                    </div>
-                  </>
-                }
-                icon={<FolderOpenIcon />}
-              >
-                {children}
-              </SubMenu>
-            );
-          })}
-        </MenuGroup>
-        <MenuGroup
-          title={
-            <div className={styles.menuGroupTitleContainer}>
-              <span>标签</span>
-              <AddIcon
-                size="large"
-                style={{ color: 'var(--td-text-color-placeholder)' }}
-                className={styles.icon}
-              />
-            </div>
-          }
-        >
-          {tags.map((item: TagItem) => (
-            <MenuItem key={item.id} value={item.id} icon={<DiscountIcon />}>
-              <div
-                className={styles.identifier}
-                itemID={item.id}
-                itemType="tagItem"
-              >
-                <EllipsisIcon
-                  className={styles.moreBtn}
-                  style={{
-                    color:
-                      active === item.id
-                        ? '#fff'
-                        : 'var(--td-text-color-primary)',
-                  }}
-                  onClick={() => {
-                    showContextMenuByClick('tagItem', item.id);
-                  }}
-                />
-              </div>
-              <div className={styles.menuItem}>
-                <div className={styles.title}>{item.title}</div>
-                <div className={styles.indicators}>
-                  <div
-                    className={styles.color}
-                    style={{ backgroundColor: item?.color }}
-                  />
-                  <div
-                    className={styles.number}
-                    style={{
-                      color:
-                        active === item.id
-                          ? '#fff'
-                          : 'var(--td-text-color-primary)',
-                    }}
-                  >
-                    0
-                  </div>
-                </div>
-              </div>
-            </MenuItem>
-          ))}
-        </MenuGroup>
-      </Menu>
-    </div>
+            ))}
+          </MenuGroup>
+        </Menu>
+      </div>
+    </>
   );
 };
 
