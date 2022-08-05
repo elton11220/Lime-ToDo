593d159d5aaaf83872dd1a0fa78a353b5e320c86
elton11220 <jonesbak@outlook.com>
2022-08-05 18:14:23 +0800

    Added a empty status prompt for todo list

--- src/renderer/components/ToDoList/index.tsx
+++ src/renderer/components/ToDoList/index.tsx
@@ -1,18 +1,20 @@
 import React, { useContext, useEffect, useMemo, useState } from 'react';
 import TodoContext from 'renderer/views/todos/context';
-// import TodoContext from 'renderer/views/todos/context';
 import { EllipsisIcon, MenuUnfoldIcon } from 'tdesign-icons-react';
 import { Space } from 'tdesign-react';
+import { SortedLinkedList } from 'utils/LinkedList';
 import NewTaskInput from '../NewTaskInput';
+import emptyTaskAnimation from '../../animations/empty-task.json';
 
 import styles from './style.module.scss';
+import Lottie from '../Lottie';
 
 interface ToDoListProps {
   title: string;
 }
 
 const ToDoList: React.FC<ToDoListProps> = (props) => {
-  const { active, tagTitles } = useContext(TodoContext);
+  const { active, tagTitles, todoItemsMap } = useContext(TodoContext);
   const { title } = props;
   const newTaskInputPlaceholder = useMemo<string>(() => {
     if (active === 'today' || active === 'recent') {
@@ -39,6 +41,13 @@ const ToDoList: React.FC<ToDoListProps> = (props) => {
   useEffect(() => {
     setNewTaskTargetId(defaultTargetId);
   }, [defaultTargetId]);
+  const todoItems = useMemo<SortedLinkedList<TodoItem>>(() => {
+    const list = todoItemsMap.get(active);
+    if (list !== null) {
+      return list;
+    }
+    return new SortedLinkedList<TodoItem>();
+  }, [active, todoItemsMap]);
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -58,6 +67,19 @@ const ToDoList: React.FC<ToDoListProps> = (props) => {
           />
         </div>
       ) : null}
+      {todoItems.length > 0 ? (
+        <div className={styles.todoItems}>
+          <div>placeholder</div>
+        </div>
+      ) : (
+        <div className={styles.emptyContainer}>
+          <Lottie size="300px" animationData={emptyTaskAnimation} />
+          <div className={styles.description}>
+            <div>没有任务，放松一下</div>
+            <div>想记点什么？点击输入框写下来</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
