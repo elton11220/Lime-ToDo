79e6c4b8a5e8609f18f32177f0316a13babe8e56
elton11220 <jonesbak@outlook.com>
2022-08-13 21:07:01 +0800

    feat: Added a tag group to the right side of the to-do list item

--- src/renderer/components/ToDoItem/index.tsx
+++ src/renderer/components/ToDoItem/index.tsx
@@ -1,6 +1,8 @@
-import { memo, useState } from 'react';
+import { memo, useContext, useState } from 'react';
 import type { FC } from 'react';
 
+import { Space, Tag, Tooltip } from 'tdesign-react';
+import TodoContext from 'renderer/views/todos/context';
 import CheckInputBox from '../CheckInputBox';
 import styles from './style.module.scss';
 
@@ -12,6 +14,16 @@ interface ToDoItemProps {
 const ToDoItem: FC<ToDoItemProps> = (props) => {
   const { sideColor, initialValue } = props;
   const [value, setValue] = useState(initialValue);
+  const { tagMap, colors } = useContext(TodoContext);
+  const removeTag = (tagId: string) => {
+    setValue((state) => ({
+      ...state,
+      data: {
+        ...state.data,
+        tags: state.data.tags.filter((tag) => tag !== tagId),
+      },
+    }));
+  };
   return (
     <div className={styles.container} style={{ borderLeftColor: sideColor }}>
       <div className={styles.main}>
@@ -29,6 +41,69 @@ const ToDoItem: FC<ToDoItemProps> = (props) => {
           }}
           priority={value.priority}
         />
+        <Space size="small" style={{ marginLeft: '8px' }}>
+          {value.data.tags.slice(0, 2).map((tagId: string) => {
+            const tagData = tagMap.get(tagId) as TagItem;
+            const element =
+              tagData.title.length > 6 ? (
+                <Tooltip
+                  content={tagData.title}
+                  showArrow={false}
+                  key={tagData.id}
+                  delay={2}
+                >
+                  <Tag
+                    shape="round"
+                    size="small"
+                    maxWidth="100px"
+                    style={{ backgroundColor: `${colors[tagData.color]}55` }}
+                    className={styles.tag}
+                  >
+                    {tagData.title}
+                    <div
+                      className={styles.removeTagBtn}
+                      onClick={() => {
+                        removeTag(tagData.id);
+                      }}
+                    >
+                      <div>
+                        <div />
+                        <div />
+                      </div>
+                    </div>
+                  </Tag>
+                </Tooltip>
+              ) : (
+                <Tag
+                  shape="round"
+                  size="small"
+                  maxWidth="100px"
+                  style={{ backgroundColor: `${colors[tagData.color]}55` }}
+                  className={styles.tag}
+                  key={tagData.id}
+                >
+                  {tagData.title}
+                  <div
+                    className={styles.removeTagBtn}
+                    onClick={() => {
+                      removeTag(tagData.id);
+                    }}
+                  >
+                    <div>
+                      <div />
+                      <div />
+                    </div>
+                  </div>
+                </Tag>
+              );
+            return element;
+          })}
+          {value.data.tags.length > 2 ? (
+            <Tag shape="round" size="small">
+              +{value.data.tags.length - 2}
+            </Tag>
+          ) : null}
+        </Space>
       </div>
       <div className={styles.detail}>
         {
